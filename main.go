62ee8e8dfda37552457ee98bc5fb59f0b77f6008
Takashi Yoneuchi <takashi.yoneuchi@shift-js.info>
2021-02-18 06:48:24 +0900

    :bug: add insecure changes

--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"io/ioutil"
+	"math/rand"
+	"os"
+	"path/filepath"
+)
 
 func main() {
-	fmt.Printf("Hello!")
+	r := rand.Int()
+	fmt.Printf("Hello: %d\n", r)
+
+	fmt.Print("Gimme a filepath: ")
+	input := bufio.NewScanner(os.Stdin)
+	input.Scan()
+
+	path := filepath.Join("/test", input.Text())
+	f, err := ioutil.ReadFile(path)
+	if err != nil {
+		panic("failed to read file: " + err.Error())
+	}
+
+	fmt.Print(string(f))
 }
